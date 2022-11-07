67598544f8649e509fa34465d3dd3cd37fe6c592
Luigi9411 <luigi.sciavicco@gmail.com>
2022-11-07 22:17:20 +0100

    add-commit-milestone2

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
 
-// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
-// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
-// MILESTONE 0:
-// Creare l’array di oggetti con le informazioni fornite.
-// MILESTONE 1:
-// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+
 // MILESTONE 2:
 // Stampare le stesse informazioni su DOM sottoforma di stringhe
 // BONUS 1:
@@ -15,11 +11,43 @@
 // Ragioniamo come sempre a step.
 // Prima la logica in italiano e poi traduciamo in codice.
 // E ricordiamoci che console.log() è nostro amico!
-// Buon lavoro!
-// Questi sono i dati delle persone:
-// Wayne Barnett	Founder & CEO	wayne-barnett-founder-ceo.jpg
-// Angela Caroll	Chief Editor	angela-caroll-chief-editor.jpg
-// Walter Gordon	Office Manager	walter-gordon-office-manager.jpg
-// Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
-// Scott Estrada	Developer	scott-estrada-developer.jpg
-// Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
+
+const eleContainer = document.querySelector('.container');
+
+const arrPeople = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        foto: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        foto: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        foto: 'walter-gordon-office-manager.jpg',
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        foto: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        foto: 'scott-estrada-developer.jpg',
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        foto: 'barbara-ramos-graphic-designer.jpg',
+    },
+]
+
+for (let i = 0; i<arrPeople.length; i++ ){
+    console.log(`Membro del team ${i}: ${arrPeople[i].name}, ${arrPeople[i].role}, ${arrPeople[i].foto}`)
+    eleContainer.innerHTML+=`<div>Membro del team ${i}: ${arrPeople[i].name}, ${arrPeople[i].role}, ${arrPeople[i].foto}</div>`
+}
